1c747424d995dc9e92cfffd8d56d528033b95042
Dominic <dominicdeantonio@icloud.com>
2020-10-28 21:16:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# deantoniodev
+
+My website
+
+## Getting Started
---
+++ android/app/src/main/kotlin/com/deantoniodev/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.deantoniodev
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
